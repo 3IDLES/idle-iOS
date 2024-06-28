33e53868d6a643539e8782b1c1ee24599fd5ef9d
J0onYEong <cjy2134@naver.com>
2024-06-28 21:44:15 +0900

    [IDLE-119] 의존성모듈 이름수정(fix)

--- project/Projects/Data/ConcreteRepository/RepositoryExample.swift
+++ project/Projects/Data/ConcreteRepository/RepositoryExample.swift
@@ -7,21 +7,4 @@
 
 import Foundation
 import RepositoryInterface
-import NetworkInterface
-import Entity
-
-public class DefaultRepository: RepositoryInterface {
-    
-    let networkDataSource: NetworkDataSource
-    
-    public init(networkDataSource: NetworkDataSource) {
-        self.networkDataSource = networkDataSource
-    }
-    
-    public func getHelloMessage() -> TestEntity {
-        
-        let str = networkDataSource.reqeust()
-        
-        return TestEntity(text: str)
-    }
-}
+import NetworkDataSource
