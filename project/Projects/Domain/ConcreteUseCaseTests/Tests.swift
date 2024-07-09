1afe96a3049956f768fcacdc3738adde90a546db
J0onYEong <cjy2134@naver.com>
2024-07-09 17:10:13 +0900

    [IDLE-72] 아이디 비밀번호 유효성 검사 테스트 코드 작성

--- project/Projects/Domain/ConcreteUseCaseTests/Tests.swift
+++ project/Projects/Domain/ConcreteUseCaseTests/Tests.swift
@@ -12,9 +12,9 @@ import XCTest
 
 final class ConcreteTests: XCTestCase {
     
+    let usecase = DefaultCenterRegisterUseCase(repository: DefaultCenterRegisterRepository())
+    
     func testPhoneNumberRegex() {
-        
-        let usecase = DefaultCenterRegisterUseCase(repository: DefaultCenterRegisterRepository())
         
         let result1 = usecase.checkPhoneNumberIsValid(
             phoneNumber: "01012341234"
@@ -33,5 +33,42 @@ final class ConcreteTests: XCTestCase {
         )
         
         XCTAssertFalse(result3, "✅ 올바른 번호 실패")
+    }
+    
+    // MARK: Id & Password
+    
+    func testValidId() {
+        // 유효한 아이디 테스트
+        XCTAssertTrue(usecase.checkIdIsValid(id: "User123"))
+        XCTAssertTrue(usecase.checkIdIsValid(id: "user12"))
+        XCTAssertTrue(usecase.checkIdIsValid(id: "123456"))
+        XCTAssertTrue(usecase.checkIdIsValid(id: "abcdef"))
+        XCTAssertTrue(usecase.checkIdIsValid(id: "ABCDEF"))
+    }
+    
+    func testInvalidId() {
+        // 유효하지 않은 아이디 테스트
+        XCTAssertFalse(usecase.checkIdIsValid(id: "Us3!")) // 너무 짧음
+        XCTAssertFalse(usecase.checkIdIsValid(id: "user@123")) // 특수 문자 포함
+        XCTAssertFalse(usecase.checkIdIsValid(id: "123456789012345678901")) // 너무 길음
+        XCTAssertFalse(usecase.checkIdIsValid(id: "user name")) // 공백 포함
+    }
+    
+    func testValidPassword() {
+        // 유효한 비밀번호 테스트
+        XCTAssertTrue(usecase.checkPasswordIsValid(password: "Password1"))
+        XCTAssertTrue(usecase.checkPasswordIsValid(password: "pass1234"))
+        XCTAssertTrue(usecase.checkPasswordIsValid(password: "1234Abcd!"))
+        XCTAssertTrue(usecase.checkPasswordIsValid(password: "Valid123"))
+        XCTAssertTrue(usecase.checkPasswordIsValid(password: "StrongPass1!"))
+    }
+    
+    func testInvalidPassword() {
+        // 유효하지 않은 비밀번호 테스트
+        XCTAssertFalse(usecase.checkPasswordIsValid(password: "short1")) // 너무 짧음
+        XCTAssertFalse(usecase.checkPasswordIsValid(password: "alllowercase")) // 숫자 없음
+        XCTAssertFalse(usecase.checkPasswordIsValid(password: "ALLUPPERCASE")) // 숫자 없음
+        XCTAssertFalse(usecase.checkPasswordIsValid(password: "12345678")) // 영문자 없음
+        XCTAssertFalse(usecase.checkPasswordIsValid(password: "123456789012345678901")) // 너무 길음
     }
 }
