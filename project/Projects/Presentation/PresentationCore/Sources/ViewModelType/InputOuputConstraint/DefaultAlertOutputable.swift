7476176683d387d717a8cbeb5afa22c1d69c8094
J0onYEong <cjy2134@naver.com>
2024-07-25 14:22:42 +0900

    [IDLE-000] DefaultAlertOutputable 재생성

---
+++ project/Projects/Presentation/PresentationCore/Sources/ViewModelType/InputOuputConstraint/DefaultAlertOutputable.swift
@@ -0,0 +1,14 @@
+//
+//  DefaultAlertOutputable.swift
+//  PresentationCore
+//
+//  Created by choijunios on 7/25/24.
+//
+
+import Entity
+import RxCocoa
+
+public protocol DefaultAlertOutputable {
+    
+    var alert: Driver<DefaultAlertContentVO> { get }
+}
