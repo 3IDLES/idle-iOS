e08b6e38a6351aaaac7807257ab48d86d8a8188a
J0onYEong <cjy2134@naver.com>
2024-07-25 18:44:30 +0900

    [IDLE-175] 커스텀 탭바 구현

--- project/Projects/Presentation/DSKit/ExampleApp/Sources/SceneDelegate.swift
+++ project/Projects/Presentation/DSKit/ExampleApp/Sources/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import DSKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
@@ -16,7 +17,38 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else { return }
         
         window = UIWindow(windowScene: windowScene)
-        let vc = AlertViewController()
+        let vc = IdleTabBar()
+        vc.setViewControllers(info: [
+            TabBarInfo(
+                viewController: UINavigationController(rootViewController: {
+                    let vc = UIViewController()
+                    vc.view.backgroundColor = .blue
+                    return vc
+                }()),
+                tabBarItem: .init(name: "홈")
+            ),
+            
+            TabBarInfo(
+                viewController: UINavigationController(rootViewController: {
+                    let vc = UIViewController()
+                    vc.view.backgroundColor = .yellow
+                    return vc
+                }()),
+                tabBarItem: .init(name: "프로필")
+            ),
+            
+            TabBarInfo(
+                viewController: UINavigationController(rootViewController: {
+                    let vc = UIViewController()
+                    vc.view.backgroundColor = .red
+                    return vc
+                }()),
+                tabBarItem: .init(name: "설정")
+            ),
+        ])
+        
+        vc.selectedIndex = 0
+        
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
