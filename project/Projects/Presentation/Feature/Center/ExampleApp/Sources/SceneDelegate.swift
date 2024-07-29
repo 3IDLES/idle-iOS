c7260185bd5f2f1fc9401a18c23fc79410acfa60
J0onYEong <cjy2134@naver.com>
2024-07-29 13:38:11 +0900

    [IDLE-171] 센터프로필DTO수정
    introduce는 nullable이다.

--- project/Projects/Presentation/Feature/Center/ExampleApp/Sources/SceneDelegate.swift
+++ project/Projects/Presentation/Feature/Center/ExampleApp/Sources/SceneDelegate.swift
@@ -23,8 +23,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let store = TestStore()
         
         try! store.saveAuthToken(
-            accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOm51bGwsInN1YiI6bnVsbCwiaXNzIjoiM2lkaW90cyIsImlhdCI6MTcyMjIyNDUzNywibmJmIjoxNzIyMjI0NTM3LCJleHAiOjE3MjIyMjUxMzcsInR5cGUiOiJBQ0NFU1NfVE9LRU4iLCJ1c2VySWQiOiIwMTkwZjI2NS0yYmVkLTc1MTEtYjljNy1hZjEwOTNhZTUzZmIiLCJwaG9uZU51bWJlciI6IjAxMC00NDQ0LTUyMzIiLCJ1c2VyVHlwZSI6ImNlbnRlciJ9.VIJ9OG6VubPNYOhSfTRt1rG_mbq6ttVNHfT1-cnjZsc",
-            refreshToken: ""
+            accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOm51bGwsInN1YiI6bnVsbCwiaXNzIjoiM2lkaW90cyIsImlhdCI6MTcyMjIyNzcxMywibmJmIjoxNzIyMjI3NzEzLCJleHAiOjE3MjIyMjgzMTMsInR5cGUiOiJBQ0NFU1NfVE9LRU4iLCJ1c2VySWQiOiIwMTkwZmNjNS01OGI1LTdlOWYtYTE3NS1hZDUwMjZjMzI4M2EiLCJwaG9uZU51bWJlciI6IjAxMC00NDQ0LTUyMzIiLCJ1c2VyVHlwZSI6ImNlbnRlciJ9.gJXEtDruIRqYM9R6aszejnIDOm8VP6ROnrNqESIdssE",
+            refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOm51bGwsInN1YiI6bnVsbCwiaXNzIjoiM2lkaW90cyIsImlhdCI6MTcyMjIyNzcxMywibmJmIjoxNzIyMjI3NzEzLCJleHAiOjE3MjM0MzczMTMsInR5cGUiOiJSRUZSRVNIX1RPS0VOIiwidXNlcklkIjoiMDE5MGZjYzUtNThiNS03ZTlmLWExNzUtYWQ1MDI2YzMyODNhIiwidXNlclR5cGUiOiJjZW50ZXIifQ.EtV-qojoAl-H7VVm-Dr2tYf6Hkbx3OdwbsxduAOFf6I"
         )
         
         let useCase = DefaultCenterProfileUseCase(
@@ -39,10 +39,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             navigationController: navigationController
         )
         
-        let vc = CenterProfileViewController()
-        vc.bind(viewModel: CenterProfileViewModel(
+        let vm = CenterProfileViewModel(
             useCase: useCase
-        ))
+        )
+        
+        let vc = CenterProfileViewController()
+        vc.bind(viewModel: vm)
         
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = vc
