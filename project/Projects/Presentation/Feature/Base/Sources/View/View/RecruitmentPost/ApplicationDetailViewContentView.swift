834e156365638db64a5645a9a818fac567cc17db
J0onYEong <cjy2134@naver.com>
2024-08-07 10:42:41 +0900

    [IDLE-104] 공고 ContentView들을 BaseFeature모듈로 이동
    재사용을 위한 이동입니다.

---
+++ project/Projects/Presentation/Feature/Base/Sources/View/View/RecruitmentPost/ApplicationDetailViewContentView.swift
@@ -0,0 +1,352 @@
+//
+//  ApplicationDetailViewContentView.swift
+//  BaseFeature
+//
+//  Created by choijunios on 8/7/24.
+//
+
+import UIKit
+import PresentationCore
+import RxCocoa
+import RxSwift
+import Entity
+import DSKit
+
+public protocol ApplicationDetailContentVMable {
+    
+    // Input
+    var experiencePreferenceType: PublishRelay<ExperiencePreferenceType> { get }
+    var applyType: PublishRelay<ApplyType> { get }
+    var applyDeadlineType: PublishRelay<ApplyDeadlineType> { get }
+    var deadlineDate: BehaviorRelay<Date?> { get }
+    
+    // Output
+    var deadlineString: Driver<String> { get }
+    var applicationDetailViewNextable: Driver<Bool> { get }
+    var casting_applicationDetail: Driver<ApplicationDetailStateObject> { get }
+}
+
+public class ApplicationDetailViewContentView: UIView {
+    
+    // Init
+    public weak var viewController: UIViewController?
+    
+    // 경력우대 여부
+    let expButtons: [StateButtonTyp1] = {
+        ExperiencePreferenceType.allCases.map { type in
+            StateButtonTyp1(
+                text: type.korTextForBtn,
+                initial: .normal
+            )
+        }
+    }()
+    
+    // 지원 방법
+    let applyTypeButtons: [StateButtonTyp1] = {
+        ApplyType.allCases.map { type in
+            StateButtonTyp1(
+                text: type.korTextForBtn,
+                initial: .normal
+            )
+        }
+    }()
+    
+    // 접수 마감 타입
+    let deadlineTypeButtons: [StateButtonTyp1] = {
+        ApplyDeadlineType.allCases.map { type in
+            StateButtonTyp1(
+                text: type.korTextForBtn,
+                initial: .normal
+            )
+        }
+    }()
+    
+    // 날짜 선택 버튼
+    let calendarOpenButton: CalendarOpenButton = {
+        let view = CalendarOpenButton()
+        view.isHidden = true
+        return view
+    }()
+    
+    
+    private let deadlineDate: PublishRelay<Date> = .init()
+    
+    private let disposeBag = DisposeBag()
+    
+    public init(viewController: UIViewController) {
+        self.viewController = viewController
+        super.init(frame: .zero)
+        setLayout()
+        setObservable()
+    }
+    
+    public required init(coder: NSCoder) { fatalError() }
+    
+    
+    func setLayout() {
+        
+        // 정적 크기
+        [
+            expButtons,
+            applyTypeButtons,
+            deadlineTypeButtons
+        ]
+        .flatMap { $0 }
+        .forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                view.widthAnchor.constraint(equalToConstant: 104),
+                view.heightAnchor.constraint(equalToConstant: 44),
+            ])
+        }
+        
+        let stackList: [VStack] = [
+        
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "경력 우대 여부"),
+                    HStack([
+                        expButtons as [UIView],
+                        [UIView()]
+                    ].flatMap({ $0 }), spacing: 4),
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+            
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "지원 방법", subTitleText: "(다중 선택 가능)"),
+                    HStack([
+                        applyTypeButtons as [UIView],
+                        [UIView()]
+                    ].flatMap({ $0 }), spacing: 4),
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+            
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "접수 마감일"),
+                    HStack([
+                        deadlineTypeButtons as [UIView],
+                        [UIView()]
+                    ].flatMap({ $0 }), spacing: 4),
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+        ]
+        
+        let scrollViewContentStack = VStack(
+            stackList,
+            spacing: 28,
+            alignment: .fill
+        )
+        
+        [
+            scrollViewContentStack,
+            calendarOpenButton
+        ].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            scrollViewContentStack.topAnchor.constraint(equalTo: self.topAnchor),
+            scrollViewContentStack.leftAnchor.constraint(equalTo: self.leftAnchor),
+            scrollViewContentStack.rightAnchor.constraint(equalTo: self.rightAnchor),
+            
+            calendarOpenButton.topAnchor.constraint(equalTo: scrollViewContentStack.bottomAnchor, constant: 12),
+            calendarOpenButton.rightAnchor.constraint(equalTo: self.rightAnchor),
+            calendarOpenButton.leftAnchor.constraint(equalTo: self.leftAnchor),
+            calendarOpenButton.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    private func setObservable() {
+        
+        deadlineTypeButtons[1]
+            .eventPublisher
+            .map({ !($0 == .accent) })
+            .bind(to: calendarOpenButton.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        calendarOpenButton
+            .rx.tap
+            .subscribe { [weak self, weak viewController] _ in
+                
+                let vc = OneDayPickerViewController()
+                vc.delegate = self
+                vc.modalPresentationStyle = .overFullScreen
+                viewController?.present(vc, animated: false)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    public func bind(viewModel: ApplicationDetailContentVMable) {
+        
+        // Output
+        
+        viewModel
+            .casting_applicationDetail
+            .drive(onNext: { [weak self] stateFromVM in
+                
+                guard let self else { return }
+                
+                // 경력 우대 여부
+                if let state = stateFromVM.experiencePreferenceType {
+                    
+                    expButtons
+                        .enumerated()
+                        .forEach { (index, button) in
+                            let item = ExperiencePreferenceType(rawValue: index)!
+                            
+                            if item == state {
+                                button.setState(.accent)
+                            }
+                        }
+                }
+                
+                // 지원 방법
+                if let state = stateFromVM.applyType {
+                    
+                    applyTypeButtons
+                        .enumerated()
+                        .forEach { (index, button) in
+                            let item = ApplyType(rawValue: index)!
+                            
+                            if item == state {
+                                button.setState(.accent)
+                            }
+                        }
+                }
+                
+                // 마감기한 방법
+                if let state = stateFromVM.applyDeadlineType {
+                    
+                    deadlineTypeButtons
+                        .enumerated()
+                        .forEach { (index, button) in
+                            let item = ApplyDeadlineType(rawValue: index)!
+                            
+                            if item == state {
+                                button.setState(.accent)
+                            }
+                        }
+                }
+                
+                // 마감기간
+                if let state = stateFromVM.deadlineDate {
+                    
+                    calendarOpenButton
+                        .textLabel.textString = state.convertDateToString()
+                }
+                
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel
+            .deadlineString
+            .drive(onNext: { [calendarOpenButton] str in
+                calendarOpenButton.textLabel.textString = str
+            })
+            .disposed(by: disposeBag)
+        
+        // Input
+        
+        Observable
+            .merge(
+                expButtons
+                    .enumerated()
+                    .map { (index, button) in
+                        let item = ExperiencePreferenceType(rawValue: index)!
+                        return button.eventPublisher
+                            .compactMap { state -> ExperiencePreferenceType? in state == .accent ? item : nil }
+                            .asObservable()
+                    }
+            )
+            .map { [expButtons] (type) in
+                
+                expButtons
+                    .enumerated()
+                    .forEach { (index, button) in
+                        let item = ExperiencePreferenceType(rawValue: index)!
+                        if item != type {
+                            button.setState(.normal)
+                        }
+                    }
+                
+                return type
+            }
+            .bind(to: viewModel.experiencePreferenceType)
+            .disposed(by: disposeBag)
+        
+        
+        Observable
+            .merge(
+                applyTypeButtons
+                    .enumerated()
+                    .map { (index, button) in
+                        let item = ApplyType(rawValue: index)!
+                        return button.eventPublisher
+                            .compactMap { state -> ApplyType? in state == .accent ? item : nil }
+                            .asObservable()
+                    }
+            )
+            .map { [applyTypeButtons] (type) in
+                
+                applyTypeButtons
+                    .enumerated()
+                    .forEach { (index, button) in
+                        let item = ApplyType(rawValue: index)!
+                        if item != type {
+                            button.setState(.normal)
+                        }
+                    }
+                
+                return type
+            }
+            .bind(to: viewModel.applyType)
+            .disposed(by: disposeBag)
+        
+        Observable
+            .merge(
+                deadlineTypeButtons
+                    .enumerated()
+                    .map { (index, button) in
+                        let item = ApplyDeadlineType(rawValue: index)!
+                        return button.eventPublisher
+                            .compactMap { state -> ApplyDeadlineType? in state == .accent ? item : nil }
+                            .asObservable()
+                    }
+            )
+            .map { [deadlineTypeButtons] (type) in
+                
+                deadlineTypeButtons
+                    .enumerated()
+                    .forEach { (index, button) in
+                        let item = ApplyDeadlineType(rawValue: index)!
+                        if item != type {
+                            button.setState(.normal)
+                        }
+                    }
+                
+                return type
+            }
+            .bind(to: viewModel.applyDeadlineType)
+            .disposed(by: disposeBag)
+        
+        deadlineDate
+            .bind(to: viewModel.deadlineDate)
+            .disposed(by: disposeBag)
+    }
+}
+
+extension ApplicationDetailViewContentView: OneDayPickerDelegate {
+    public func oneDayPicker(selectedDate: Date) {
+        // 위임자 패턴으로 데이터를 수신
+        deadlineDate.accept(selectedDate)
+    }
+}
---
+++ project/Projects/Presentation/Feature/Base/Sources/View/View/RecruitmentPost/CustomerInformationContentView.swift
@@ -0,0 +1,385 @@
+//
+//  CustomerInformationContentView.swift
+//  BaseFeature
+//
+//  Created by choijunios on 8/7/24.
+//
+
+import UIKit
+import PresentationCore
+import RxCocoa
+import RxSwift
+import Entity
+import DSKit
+
+public protocol CustomerInformationContentVMable {
+    var name: PublishRelay<String> { get }
+    var gender: PublishRelay<Gender> { get }
+    var birthYear: PublishRelay<String> { get }
+    var weight: PublishRelay<String> { get }
+    var careGrade: PublishRelay<CareGrade> { get }
+    var cognitionState: PublishRelay<CognitionDegree> { get }
+    var deceaseDescription: PublishRelay<String> { get }
+    
+    var casting_customerInformation: Driver<CustomerInformationStateObject> { get }
+    var customerInformationNextable: Driver<Bool> { get }
+}
+
+public class CustomerInformationContentView: UIView {
+    
+    // 이름 입력
+    public lazy var nameField: MultiLineTextField = {
+        let field = MultiLineTextField(typography: .Body3, placeholderText: "고객의 이름을 입력해 주세요.")
+        field.isScrollEnabled = false
+        return field
+    }()
+    
+    // 성별 선택
+    let maleBtn: StateButtonTyp1 = {
+        let btn = StateButtonTyp1(text: "남성", initial: .normal)
+        btn.label.typography = .Body3
+        return btn
+    }()
+    let femaleBtn: StateButtonTyp1 = {
+        let btn = StateButtonTyp1(text: "여성", initial: .normal)
+        btn.label.typography = .Body3
+        return btn
+    }()
+    
+    // 출생연도
+    public lazy var birthYearField: MultiLineTextField = {
+        let field = MultiLineTextField(typography: .Body3, placeholderText: "고객의 출생연도를 입력해주세요. (예: 1965)")
+        field.isScrollEnabled = false
+        field.keyboardType = .numberPad
+        return field
+    }()
+    
+    // 몸무게
+    public lazy var weightField: TextFieldWithDegree = {
+        let field = TextFieldWithDegree(
+            degreeText: "kg",
+            initialText: ""
+        )
+        field.textField.keyboardType = .numberPad
+        return field
+    }()
+    
+    // 요양등급
+    let careGradeButtons: [StateButtonTyp1] = {
+        CareGrade.allCases.map { grade in
+            StateButtonTyp1(
+                text: grade.textForCellBtn,
+                initial: .normal
+            )
+        }
+    }()
+    
+    // 인지상태
+    let cognitionButtons: [StateButtonTyp1] = {
+        CognitionDegree.allCases.map { cognition in
+            StateButtonTyp1(
+                text: cognition.korTextForCellBtn,
+                initial: .normal
+            )
+        }
+    }()
+    
+    // 질병
+    public lazy var deceaseField: MultiLineTextField = {
+        let field = MultiLineTextField(typography: .Body3, placeholderText: "고객이 현재 앓고 있는 질병 또는 병력을 입력해주세요.")
+        field.isScrollEnabled = false
+        return field
+    }()
+    
+    private let disposeBag = DisposeBag()
+    
+    public init() {
+        super.init(frame: .zero)
+        setLayout()
+    }
+    
+    public required init(coder: NSCoder) { fatalError() }
+    
+    
+    func setLayout() {
+        
+        // 정적 크기
+        [
+            [maleBtn, femaleBtn],
+            cognitionButtons
+        ]
+        .flatMap { $0 }
+        .forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                view.widthAnchor.constraint(equalToConstant: 104),
+                view.heightAnchor.constraint(equalToConstant: 44),
+            ])
+        }
+        
+        careGradeButtons
+            .forEach { view in
+                view.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                    view.widthAnchor.constraint(equalToConstant: 40),
+                    view.heightAnchor.constraint(equalToConstant: 40),
+                ])
+            }
+        
+        let stackList: [VStack] = [
+        
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "이름"),
+                    nameField
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+            
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "성별"),
+                    HStack([maleBtn, femaleBtn, UIView()], spacing: 4),
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+            
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "출생연도"),
+                    birthYearField
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+            
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "몸무게"),
+                    weightField
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+            
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "요양 등급"),
+                    HStack([
+                        careGradeButtons as [UIView],
+                        [UIView()]
+                    ].flatMap({ $0 }), spacing: 4),
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+            
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "인지상태"),
+                    HStack([
+                        cognitionButtons as [UIView],
+                        [UIView()]
+                    ].flatMap({ $0 }), spacing: 4),
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+            
+            VStack(
+                [
+                    IdleContentTitleLabel(titleText: "질병", subTitleText: "(선택)"),
+                    deceaseField
+                ],
+                spacing: 6,
+                alignment: .fill
+            ),
+        ]
+        
+        let scrollViewContentStack = VStack(
+            stackList,
+            spacing: 28,
+            alignment: .fill
+        )
+        
+        [
+            scrollViewContentStack
+        ].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            scrollViewContentStack.topAnchor.constraint(equalTo: self.topAnchor),
+            scrollViewContentStack.leftAnchor.constraint(equalTo: self.leftAnchor),
+            scrollViewContentStack.rightAnchor.constraint(equalTo: self.rightAnchor),
+            scrollViewContentStack.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+    }
+    
+    public func bind(viewModel: CustomerInformationContentVMable) {
+        
+        // output
+        viewModel
+            .casting_customerInformation
+            .drive(onNext: { [weak self] stateFromVM in
+              
+                guard let self else { return }
+                
+                // 이름
+                if !stateFromVM.name.isEmpty {
+                    nameField.textString = stateFromVM.name
+                }
+                
+                // 성별
+                if let gender = stateFromVM.gender {
+                    if gender == .male {
+                        maleBtn.setState(.accent)
+                    } else {
+                        femaleBtn.setState(.accent)
+                    }
+                }
+                
+                // 출생년도
+                if !stateFromVM.birthYear.isEmpty {
+                    birthYearField.textString = stateFromVM.birthYear
+                }
+                
+                // 몸무게
+                if !stateFromVM.weight.isEmpty {
+                    weightField.textField.textString = stateFromVM.weight
+                }
+                
+                // 요양등급
+                if let state = stateFromVM.careGrade {
+                    
+                    careGradeButtons
+                        .enumerated()
+                        .forEach { (index, button) in
+                            let item = CareGrade(rawValue: index)!
+                            
+                            if item == state {
+                                button.setState(.accent)
+                            }
+                        }
+                }
+                
+                // 인지상태
+                if let state = stateFromVM.cognitionState {
+                    
+                    cognitionButtons
+                        .enumerated()
+                        .forEach { (index, button) in
+                            let item = CognitionDegree(rawValue: index)!
+                            
+                            if item == state {
+                                button.setState(.accent)
+                            }
+                        }
+                }
+                
+                // 질병
+                if !stateFromVM.deceaseDescription.isEmpty {
+                    deceaseField.textString = stateFromVM.deceaseDescription
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        // Input
+        nameField.rx.text
+            .compactMap { $0 }
+            .bind(to: viewModel.name)
+            .disposed(by: disposeBag)
+        
+        Observable
+            .merge(
+                maleBtn.eventPublisher
+                    .compactMap { state -> Gender? in state == .accent ? .male : nil },
+                femaleBtn.eventPublisher
+                    .compactMap { state -> Gender? in state == .accent ? .female : nil }
+            ).map { [maleBtn, femaleBtn] (selectedGender) in
+                
+                if selectedGender == .male {
+                    femaleBtn.setState(.normal)
+                } else {
+                    maleBtn.setState(.normal)
+                }
+                
+                return selectedGender
+            }
+            .bind(to: viewModel.gender)
+            .disposed(by: disposeBag)
+        
+        birthYearField.rx.text
+            .compactMap { $0 }
+            .bind(to: viewModel.birthYear)
+            .disposed(by: disposeBag)
+        
+        weightField.textField.rx.text
+            .compactMap { $0 }
+            .bind(to: viewModel.weight)
+            .disposed(by: disposeBag)
+        
+        Observable
+            .merge(
+                careGradeButtons
+                    .enumerated()
+                    .map { (index, button) in
+                        let item = CareGrade(rawValue: index)!
+                        return button.eventPublisher
+                            .compactMap { state -> CareGrade? in state == .accent ? item : nil }
+                            .asObservable()
+                    }
+            )
+            .map { [careGradeButtons] (grade) in
+                
+                careGradeButtons
+                    .enumerated()
+                    .forEach { (index, button) in
+                        let item = CareGrade(rawValue: index)!
+                        if item != grade {
+                            button.setState(.normal)
+                        }
+                    }
+                
+                return grade
+            }
+            .bind(to: viewModel.careGrade)
+            .disposed(by: disposeBag)
+        
+        Observable
+            .merge(
+                cognitionButtons
+                    .enumerated()
+                    .map { (index, button) in
+                        let item = CognitionDegree(rawValue: index)!
+                        return button.eventPublisher
+                            .compactMap { state -> CognitionDegree? in state == .accent ? item : nil }
+                            .asObservable()
+                    }
+            )
+            .map { [cognitionButtons] (grade) in
+                
+                cognitionButtons
+                    .enumerated()
+                    .forEach { (index, button) in
+                        let item = CognitionDegree(rawValue: index)!
+                        if item != grade {
+                            button.setState(.normal)
+                        }
+                    }
+                
+                return grade
+            }
+            .bind(to: viewModel.cognitionState)
+            .disposed(by: disposeBag)
+        
+        
+        deceaseField.rx.text
+            .compactMap { $0 }
+            .bind(to: viewModel.deceaseDescription)
+            .disposed(by: disposeBag)
+    }
+}
