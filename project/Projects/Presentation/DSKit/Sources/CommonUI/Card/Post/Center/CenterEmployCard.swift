d3d2defa39a39bf9ce53555741847d786a09fefd
J0onYEong <cjy2134@naver.com>
2024-08-12 11:41:58 +0900

    [IDLE-141] CenterEmployCard 컴포넌트 구현

--- project/Projects/Presentation/DSKit/Sources/CommonUI/Card/Post/Center/CenterEmployCard.swift
+++ project/Projects/Presentation/DSKit/Sources/CommonUI/Card/Post/Center/CenterEmployCard.swift
@@ -10,6 +10,53 @@ import RxSwift
 import RxCocoa
 import Entity
 
+public struct CenterEmployCardRO {
+    public let startDay: String
+    public let endDay: String
+    public let postTitle: String
+    public let nameText: String
+    public let careGradeText: String
+    public let ageText: String
+    public let genderText: String
+    public let applicantCount: Int
+    
+    public init(startDay: String, endDay: String, postTitle: String, nameText: String, careGradeText: String, ageText: String, genderText: String, applicantCount: Int) {
+        self.startDay = startDay
+        self.endDay = endDay
+        self.postTitle = postTitle
+        self.nameText = nameText
+        self.careGradeText = careGradeText
+        self.ageText = ageText
+        self.genderText = genderText
+        self.applicantCount = applicantCount
+    }
+    
+    static let mock: CenterEmployCardRO = .init(
+        startDay: "2024. 07. 10",
+        endDay: "2024. 07. 31",
+        postTitle: "서울특별시 강남구 신사동",
+        nameText: "홍길동",
+        careGradeText: "1등급",
+        ageText: "78세",
+        genderText: "여성",
+        applicantCount: 2
+    )
+}
+
+public protocol CenterEmployCardViewModelable {
+    
+    // Output
+    var renderObject: Driver<CenterEmployCardRO>? { get }
+    
+    // Input
+    var postCardClicked: PublishRelay<Void> { get }
+    
+    // - Buttons
+    var checkApplicantBtnClicked: PublishRelay<Void> { get }
+    var editPostBtnClicked: PublishRelay<Void> { get }
+    var terminatePostBtnClicked: PublishRelay<Void> { get }
+}
+
 public class CenterEmployCard: TappableUIView {
     
     // Init
@@ -19,38 +66,56 @@ public class CenterEmployCard: TappableUIView {
     // Row1
     let durationLabel: IdleLabel = {
         let label = IdleLabel(typography: .Body3)
-        label.textColor = DSKitAsset.Colors.gray300.color
+        label.attrTextColor = DSKitAsset.Colors.gray300.color
         return label
     }()
     
     // Row2
-    let poseTitleLabel: IdleLabel = {
+    let postTitleLabel: IdleLabel = {
         let label = IdleLabel(typography: .Subtitle2)
-        label.textColor = DSKitAsset.Colors.gray300.color
         return label
     }()
     
     // Row3
     let nameLabel: IdleLabel = {
         let label = IdleLabel(typography: .Body2)
-        label.textColor = DSKitAsset.Colors.gray500.color
+        label.attrTextColor = DSKitAsset.Colors.gray500.color
         return label
     }()
     let informationLabel: IdleLabel = {
         let label = IdleLabel(typography: .Body2)
-        label.textColor = DSKitAsset.Colors.gray500.color
+        label.attrTextColor = DSKitAsset.Colors.gray500.color
         return label
     }()
     
     // Row4
-    let checkApplyButton: IdlePrimaryCardButton = {
+    let checkApplicantsButton: IdlePrimaryCardButton = {
         let btn = IdlePrimaryCardButton(level: .medium)
         btn.label.textString = ""
         return btn
     }()
     
     // Row5
-    
+    let editPostButton: ImagePrefixButton = {
+        let button = ImagePrefixButton(
+            iconImage: DSKitAsset.Icons.postEdit.image
+        )
+        button.icon.tintColor = DSKitAsset.Colors.gray300.color
+        button.label.textString = "공고 수정"
+        button.label.attrTextColor = DSKitAsset.Colors.gray500.color
+        
+        return button
+    }()
+    let terminatePostButton: ImagePrefixButton = {
+        let button = ImagePrefixButton(
+            iconImage: DSKitAsset.Icons.postCheck.image
+        )
+        button.icon.tintColor = DSKitAsset.Colors.gray300.color
+        button.label.textString = "채용 종료"
+        button.label.attrTextColor = DSKitAsset.Colors.gray500.color
+        
+        return button
+    }()
     
     
     // Observable
@@ -58,20 +123,138 @@ public class CenterEmployCard: TappableUIView {
     
     public override init() {
         super.init()
+        
+        setAppearance()
+        setLayout()
+        setObservable()
     }
     
     public required init?(coder: NSCoder) { fatalError() }
     
     private func setAppearance() {
-        
+        self.backgroundColor = .white
+        self.layer.borderColor = DSKitAsset.Colors.gray100.color.cgColor
+        self.layer.borderWidth = 1.0
+        self.layer.cornerRadius = 12
     }
     
     private func setLayout() {
         
+        self.layoutMargins = .init(top: 16, left: 16, bottom: 16, right: 16)
+        
+        // InfoLabel
+        let divider = UIView()
+        divider.backgroundColor = DSKitAsset.Colors.gray300.color
+        let infoStack = HStack([
+            nameLabel,
+            divider,
+            informationLabel,
+        ], spacing: 8)
+            
+        NSLayoutConstraint.activate([
+            divider.widthAnchor.constraint(equalToConstant: 1),
+            divider.topAnchor.constraint(equalTo: infoStack.topAnchor, constant: 5),
+            divider.bottomAnchor.constraint(equalTo: infoStack.bottomAnchor, constant: -5),
+        ])
+        
+        let textStack = VStack([
+            durationLabel,
+            Spacer(height: 4),
+            postTitleLabel,
+            Spacer(height: 2),
+            infoStack,
+        ], alignment: .leading)
+        
+        let buttonStack = HStack([
+            editPostButton,
+            terminatePostButton,
+        ], spacing: 4)
+        
+        let contentStack = VStack([
+            HStack([textStack, Spacer()]),
+            Spacer(height: 12),
+            checkApplicantsButton,
+            HStack([buttonStack, Spacer()])
+        ], alignment: .fill)
+        
+        [
+            contentStack
+        ].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            contentStack.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor),
+            contentStack.leftAnchor.constraint(equalTo: self.layoutMarginsGuide.leftAnchor),
+            contentStack.rightAnchor.constraint(equalTo: self.layoutMarginsGuide.rightAnchor),
+            contentStack.bottomAnchor.constraint(equalTo: self.layoutMarginsGuide.bottomAnchor),
+        ])
     }
     
-    private func setObservable() {
+    private func setObservable() { }
+    
+    public func binc(viewModel: CenterEmployCardViewModelable) {
+        // Output
+        viewModel
+            .renderObject?
+            .drive(onNext: { [weak self] ro in
+                // public let applicantCount: Int
+                guard let self else { return }
+                durationLabel.textString = "\(ro.startDay) ~ \(ro.endDay)"
+                postTitleLabel.textString = ro.postTitle
+                nameLabel.textString = ro.nameText
+                informationLabel.textString = "\(ro.careGradeText) \(ro.ageText) \(ro.genderText)"
+                checkApplicantsButton.label.textString = "지원자 \(ro.applicantCount)명 조회"
+            })
+            .disposed(by: disposeBag)
         
+        // Input
+        [
+            self.rx.tap
+                .bind(to: viewModel.postCardClicked),
+            
+            checkApplicantsButton
+                .rx.tap
+                .bind(to: viewModel.checkApplicantBtnClicked),
+            
+            editPostButton
+                .rx.tap
+                .bind(to: viewModel.editPostBtnClicked),
+                
+            terminatePostButton
+                .rx.tap
+                .bind(to: viewModel.terminatePostBtnClicked),
+        ].forEach {
+            $0.disposed(by: disposeBag)
+        }
+        
+        (viewModel as? TextVM)?.publishObect.accept(.mock)
     }
 }
 
+fileprivate class TextVM: CenterEmployCardViewModelable {
+
+    public let publishObect: PublishRelay<CenterEmployCardRO> = .init()
+    
+    var renderObject: RxCocoa.Driver<CenterEmployCardRO>?
+    
+    var postCardClicked: RxRelay.PublishRelay<Void> = .init()
+    var checkApplicantBtnClicked: RxRelay.PublishRelay<Void> = .init()
+    var editPostBtnClicked: RxRelay.PublishRelay<Void> = .init()
+    var terminatePostBtnClicked: RxRelay.PublishRelay<Void> = .init()
+    
+    init() {
+        
+        renderObject = publishObect.asDriver(onErrorJustReturn: .mock)
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview("Preview", traits: .defaultLayout) {
+    let btn = CenterEmployCard()
+    let vm = TextVM()
+    btn.binc(viewModel: vm)
+    
+    return btn
+}
