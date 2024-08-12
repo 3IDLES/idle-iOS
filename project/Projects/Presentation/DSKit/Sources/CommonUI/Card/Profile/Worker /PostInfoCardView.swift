22bcc3f83d5345b39ce95af2f19fc98a9171f089
J0onYEong <cjy2134@naver.com>
2024-08-12 14:15:38 +0900

    [IDLE-141] PostInfoCardView 컴포넌트 구현
    지원자 확인하기 화면에쓰이는 컴포넌트 입니다.

---
+++ project/Projects/Presentation/DSKit/Sources/CommonUI/Card/Profile/Worker /PostInfoCardView.swift
@@ -0,0 +1,79 @@
+//
+//  WorkerInfoCardView.swift
+//  DSKit
+//
+//  Created by choijunios on 8/12/24.
+//
+
+import UIKit
+import PresentationCore
+import RxCocoa
+import RxSwift
+import Entity
+
+public class PostInfoCardView: TappableUIView {
+    
+    // View
+    let contentView = CenterEmployCardInfoView()
+    
+    // Observable
+    private let disposeBag = DisposeBag()
+    
+    public override init() {
+        super.init()
+        
+        setAppearance()
+        setLayout()
+        setObservable()
+    }
+    
+    public required init?(coder: NSCoder) { fatalError() }
+    
+    private func setAppearance() {
+        self.backgroundColor = .white
+        self.layer.borderColor = DSKitAsset.Colors.gray100.color.cgColor
+        self.layer.borderWidth = 1.0
+        self.layer.cornerRadius = 12
+    }
+    
+    private func setLayout() {
+        self.layoutMargins = .init(
+            top: 16,
+            left: 16,
+            bottom: 16,
+            right: 16
+        )
+        
+        let mainStack = VStack([
+            HStack([contentView, Spacer()])
+        ],alignment: .fill)
+        
+        [
+            mainStack
+        ].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor),
+            mainStack.leftAnchor.constraint(equalTo: self.layoutMarginsGuide.leftAnchor),
+            mainStack.rightAnchor.constraint(equalTo: self.layoutMarginsGuide.rightAnchor),
+            mainStack.bottomAnchor.constraint(equalTo: self.layoutMarginsGuide.bottomAnchor),
+        ])
+    }
+    
+    private func setObservable() { }
+}
+
+@available(iOS 17.0, *)
+#Preview("Preview", traits: .defaultLayout) {
+    
+    let view = PostInfoCardView()
+    view.contentView.durationLabel.textString = "2024. 07. 10 ~ 2024. 07. 31"
+    view.contentView.informationLabel.textString = "1등급 78세 여성"
+    view.contentView.nameLabel.textString = "홍길동"
+    view.contentView.postTitleLabel.textString = "서울특별시 강남구 신사동"
+    
+    return view
+}
