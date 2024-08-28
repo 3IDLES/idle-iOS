10919eb4e1d9a8c511ab16fd38b7ef18dd3f0657
J0onYEong <cjy2134@naver.com>
2024-08-29 01:53:44 +0900

    [IDLE-000] 진행중인 공고 확인창에서 공고 닫기 기능 구현

---
+++ project/Projects/Presentation/PresentationCore/Sources/ScreenCoordinating/Notification/RecruitmentPostNotifications.swift
@@ -0,0 +1,13 @@
+//
+//  RecruitmentPost.swift
+//  PresentationCore
+//
+//  Created by choijunios on 8/29/24.
+//
+
+import Foundation
+
+public extension Notification.Name {
+    
+    static let removePostRequestFromCell: Notification.Name = .init("removePostRequestFromCell")
+}
