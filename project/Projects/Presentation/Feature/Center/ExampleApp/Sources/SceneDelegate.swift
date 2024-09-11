a750aa5a6e3b0cbca02fe415fc3b03c578fb4673
J0onYEong <cjy2134@naver.com>
2024-09-11 11:25:03 +0900

    [IDLE-000] AuthStateDisplayVC구현

--- project/Projects/Presentation/Feature/Center/ExampleApp/Sources/SceneDelegate.swift
+++ project/Projects/Presentation/Feature/Center/ExampleApp/Sources/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         navigationController.setNavigationBarHidden(true, animated: false)
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = CenterAuthVC()
+        window?.rootViewController = AuthStateDisplayVC(state: .requestCenterInfo)
         window?.makeKeyAndVisible()
         
 
