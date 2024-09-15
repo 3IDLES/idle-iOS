ef1262a1cc4b2e62931725de0d3df3e17df65a61
J0onYEong <cjy2134@naver.com>
2024-09-15 13:36:59 +0900

    [IDLE-000] SettingAdditionalInfoType구현

---
+++ project/Projects/Domain/ConcreteUseCase/Setting/SettingAdditionalInfoType.swift
@@ -0,0 +1,32 @@
+//
+//  SettingAdditionalInfoType.swift
+//  Entity
+//
+//  Created by choijunios on 9/15/24.
+//
+
+import Foundation
+
+public enum SettingAdditionalInfoType {
+    
+    case frequentQuestion
+    case contact
+    case termsandPolicies
+    case privacyPolicy
+    
+    public func getWebUrl() -> URL {
+        var urlString: String!
+        switch self {
+        case .frequentQuestion:
+            urlString = "https://grove-maraca-55d.notion.site/8579186ee8ca4dbb8dc55e3b8b744d11?pvs=4"
+        case .contact:
+            urlString = "https://docs.google.com/forms/d/1I1N9xmYuqlpy8K1ga8xOECPZl-1sW33WrKIzXas74Is/viewform?edit_requested=true"
+        case .termsandPolicies:
+            urlString = "https://grove-maraca-55d.notion.site/2e4d597aff1f406e9164cdb6f9195de0?pvs=4"
+        case .privacyPolicy:
+            urlString = "https://grove-maraca-55d.notion.site/ad4f62dff5304d63a162f1269639afca?pvs=4"
+        }
+        
+        return URL(string: urlString)!
+    }
+}
