4a7ea0c6958e7733f4adad00b0998f3571b6b8bb
J0onYEong <cjy2134@naver.com>
2024-09-16 14:04:40 +0900

    [IDLE-000] PrevOrNextContainer구현

---
+++ project/Projects/Presentation/DSKit/Sources/CommonUI/Button/PrevOrNextContainer.swift
@@ -0,0 +1,48 @@
+//
+//  PrevOrNextContainer.swift
+//  DSKit
+//
+//  Created by choijunios on 9/16/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+public class PrevOrNextContainer: HStack {
+    
+    // View
+    private let prevButton: IdleThirdinaryButton = {
+        let button = IdleThirdinaryButton(level: .medium)
+        button.label.textString = "이전"
+        return button
+    }()
+    
+    private let nextButton: IdlePrimaryButton = {
+        let button = IdlePrimaryButton(level: .medium)
+        button.label.textString = "다음"
+        return button
+    }()
+    
+    public lazy var nextBtnClicked: Driver<Void> = nextButton.rx.tap.asDriver()
+    public lazy var prevBtnClicked: Driver<Void> = prevButton.rx.tap.asDriver()
+    
+    public init() {
+        super.init(
+            [
+                prevButton,
+                nextButton
+            ],
+            spacing: 8,
+            alignment: .fill,
+            distribution: .fillEqually
+        )
+    }
+    public required init(coder: NSCoder) { fatalError() }
+}
+
+@available(iOS 17.0, *)
+#Preview("Preview", traits: .defaultLayout) {
+    
+    PrevOrNextContainer()
+}
