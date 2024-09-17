e6df890b23c81fba0173fea416634ffe05628414
J0onYEong <cjy2134@naver.com>
2024-09-18 03:22:55 +0900

    [IDLE-000] 동작별 완료까지 걸린 시간 기록기능 구현

--- project/Projects/App/ConcreteLogger/OverallLogger/OverallLogger.swift
+++ project/Projects/App/ConcreteLogger/OverallLogger/OverallLogger.swift
@@ -8,11 +8,13 @@
 import Foundation
 import LoggerInterface
 import AuthFeature
+import CenterFeature
 import Entity
 
 public protocol OverallLogger:
     CenterRegisterLogger, 
-    WorkerRegisterLogger {
+    WorkerRegisterLogger,
+    PostRegisterLogger {
     
 }
 
@@ -26,7 +28,8 @@ public class DefaultOverallLogger {
 }
 
 extension DefaultOverallLogger: OverallLogger {
-    
+
+    // MARK: Auth
     public func logCenterRegisterStep(stepName: String, stepIndex: Int) {
         
         let event = LoggingEvent(type: .screenView)
@@ -37,6 +40,21 @@ extension DefaultOverallLogger: OverallLogger {
         publisher.send(event: event)
     }
     
+    public func startCenterRegister() {
+        publisher.startTimer(
+            screenName: "center_signup",
+            actionName: "completion_duration"
+        )
+    }
+    
+    public func logCenterRegisterDuration() {
+        publisher.endTimer(
+            screenName: "center_signup",
+            actionName: "completion_duration",
+            isSuccess: true
+        )
+    }
+    
     public func logWorkerRegisterStep(stepName: String, stepIndex: Int) {
         let event = LoggingEvent(type: .screenView)
             .addValue(.screenName, value: "worker_signup")
@@ -44,5 +62,45 @@ extension DefaultOverallLogger: OverallLogger {
             .addValue(.step, value: stepIndex+1)
         
         publisher.send(event: event)
+    }
+    
+    public func startWorkerRegister() {
+        publisher.startTimer(
+            screenName: "worker_signup",
+            actionName: "completion_duration"
+        )
+    }
+    
+    public func logWorkerRegisterDuration() {
+        publisher.endTimer(
+            screenName: "worker_signup",
+            actionName: "completion_duration",
+            isSuccess: true
+        )
+    }
+    
+    // MARK: Recruitment post
+    public func logPostRegisterStep(stepName: String, stepIndex: Int) {
+        let event = LoggingEvent(type: .screenView)
+            .addValue(.screenName, value: "post_job_posting")
+            .addValue(.actionName, value: "post_job_posting" + stepName)
+            .addValue(.step, value: stepIndex+1)
+        
+        publisher.send(event: event)
+    }
+    
+    public func startPostRegister() {
+        publisher.startTimer(
+            screenName: "post_job_posting",
+            actionName: "completion_duration"
+        )
+    }
+    
+    public func logPostRegisterDuration() {
+        publisher.endTimer(
+            screenName: "post_job_posting",
+            actionName: "completion_duration",
+            isSuccess: true
+        )
     }
 }
--- project/Projects/Presentation/Feature/Auth/Sources/Logger/WorkerRegisterLogger.swift
+++ project/Projects/Presentation/Feature/Auth/Sources/Logger/WorkerRegisterLogger.swift
@@ -9,6 +9,13 @@ import Foundation
 
 public protocol WorkerRegisterLogger {
     
+    /// 요양보호사 회원가입 각 화면 진입시 로깅
     func logWorkerRegisterStep(stepName: String, stepIndex: Int)
+    
+    /// 요양보호사 회원가입의 시작시간을 기록합니다.
+    func startWorkerRegister()
+    
+    /// 요양보호사 회원가입의 완료시간을 기록하고 걸린시간을 산출합니다.
+    func logWorkerRegisterDuration()
 }
 
