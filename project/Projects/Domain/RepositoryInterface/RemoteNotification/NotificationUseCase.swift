df5e135e56089d18957c802ef4601ae21e83e8a4
J0onYEong <cjy2134@naver.com>
2024-09-26 12:52:20 +0900

    [IDLE-393] NotificationUseCase인터페이스 생성

---
+++ project/Projects/Domain/RepositoryInterface/RemoteNotification/NotificationUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationUseCase.swift
+//  UseCaseInterface
+//
+//  Created by choijunios on 9/26/24.
+//
+
+import Foundation
+
+public protocol NotificationUseCase {
+    
+    /// 유저와 매치되는 노티피케이션 토큰을 서버로 전송합니다.
+    func setNotificationToken(token: String, completion: @escaping (Bool) -> ())
+    
+    /// 유저와 매치되는 노티피케이션 토큰을 서버로부터 제거합니다.
+    func deleteNotificationToken(completion: @escaping (Bool) -> ())
+}
