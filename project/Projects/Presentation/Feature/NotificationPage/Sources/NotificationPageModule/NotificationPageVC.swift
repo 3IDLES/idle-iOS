bf3cc14a5bcaa5057c28d2513b7cf89a8bb341a7
J0onYEong <cjy2134@naver.com>
2024-09-28 18:42:20 +0900

    [IDLE-397] NotificationPageVC구현

--- project/Projects/Presentation/Feature/NotificationPage/Sources/NotificationPageModule/NotificationPageVC.swift
+++ project/Projects/Presentation/Feature/NotificationPage/Sources/NotificationPageModule/NotificationPageVC.swift
@@ -15,28 +15,45 @@ import RxSwift
 import RxCocoa
 
 
-public class NotificationPageVC: BaseViewController {
+protocol NotificationPageViewModelable: BaseViewModel {
+    
+    // Input
+    var viewWillAppear: PublishSubject<Void> { get }
+    
+    // Output
+    var tableData: Driver<[SectionInfo: [NotificationCellInfo]]>? { get }
+    
+    /// Cell ViewModel생성
+    func createCellVM(info: NotificationCellInfo) -> NotificationCellViewModelable
+}
+
+enum SectionInfo: Int, CaseIterable {
+    case today
+    case week
+    case month
+    
+    var korTwoLetterName: String {
+        switch self {
+        case .today:
+            "오늘"
+        case .week:
+            "최근 7일"
+        case .month:
+            "최근 30일"
+        }
+    }
+}
+
+
+class NotificationPageVC: BaseViewController {
     
     typealias Cell = NotificationCell
     
-    enum SectionInfo: Int, CaseIterable {
-        case today
-        case week
-        case month
-        
-        var korTwoLetterName: String {
-            switch self {
-            case .today:
-                "오늘"
-            case .week:
-                "최근 7일"
-            case .month:
-                "최근 30일"
-            }
-        }
-    }
-    
     // Init
+    
+    
+    // Table Data
+    private var tableData: [SectionInfo: [NotificationCellInfo]] = [:]
     
     // View
     let navigationBar: IdleNavigationBar = {
@@ -50,21 +67,32 @@ public class NotificationPageVC: BaseViewController {
         return tableView
     }()
     
-    public init() {
+    init(viewModel: NotificationPageViewModelable) {
         super.init(nibName: nil, bundle: nil)
+        
+        bindViewModel(viewModel: viewModel)
+        
         setUpTableView()
     }
     
-    public required init?(coder: NSCoder) { fatalError() }
+    required init?(coder: NSCoder) { fatalError() }
     
     private func setUpTableView() {
         
         // MARK: DataSource
-        tableViewDataSource = .init(tableView: tableView, cellProvider: { tableView, indexPath, itemIdentifier in
+        tableViewDataSource = .init(tableView: tableView, cellProvider: { [weak self] tableView, indexPath, itemIdentifier in
             
+            guard let self else { return Cell() }
             
+            let cell = tableView.dequeueReusableCell(withIdentifier: Cell.identifier) as! Cell
+            let vm = (viewModel as! NotificationPageViewModelable)
+            let section = SectionInfo(rawValue: indexPath.section)!
+            let cellInfo = tableData[section]![indexPath.row]
+            let cellViewModel = vm.createCellVM(info: cellInfo)
             
-            return nil
+            cell.bind(viewModel: cellViewModel)
+            
+            return cell
         })
         tableView.dataSource = tableViewDataSource
         tableView.delegate = self
@@ -113,27 +141,61 @@ public class NotificationPageVC: BaseViewController {
     }
     
     private func setObservable() { }
+    
+    private func bindViewModel(viewModel: NotificationPageViewModelable) {
+        self.bind(viewModel: viewModel)
+        
+        // Input
+        self.rx
+            .viewWillAppear
+            .map { _ in }
+            .bind(to: viewModel.viewWillAppear)
+            .disposed(by: disposeBag)
+        
+        // Output
+        viewModel
+            .tableData?
+            .drive(onNext: { [weak self] tableData in
+                
+                guard let self else { return }
+                
+                self.tableData = tableData
+                
+                var snapShot: NSDiffableDataSourceSnapshot<Int, String> = .init()
+                let sections = tableData.keys.map({ $0.rawValue })
+                
+                snapShot.appendSections(sections)
+                
+                tableData.forEach { (section, items) in
+                    let itemIds = items.map({ $0.id })
+                    snapShot.appendItems(itemIds, toSection: section.rawValue)
+                }
+                
+                tableViewDataSource.apply(snapShot)
+            })
+            .disposed(by: disposeBag)
+    }
 }
 
 // MARK: Header
 extension NotificationPageVC: UITableViewDelegate {
     
-    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let titleText = SectionInfo(rawValue: section)!
         return NotificationSectionHeader(titleText: titleText.korTwoLetterName)
     }
     
-    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         52
     }
     
-    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let footerView = UIView()
         footerView.backgroundColor = DSColor.gray050.color
         return footerView
     }
     
-    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         switch section {
         case tableView.numberOfSections-1:
             return 0
