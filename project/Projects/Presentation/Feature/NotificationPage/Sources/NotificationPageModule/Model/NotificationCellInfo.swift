59edcee05e4e8644ca9a8ebf0c54eb47050e7938
J0onYEong <cjy2134@naver.com>
2024-09-28 17:27:57 +0900

    [IDLE-000] DefualtNotificationCellViewModel 구현

--- project/Projects/Presentation/Feature/NotificationPage/Sources/NotificationPageModule/Model/NotificationCellInfo.swift
+++ project/Projects/Presentation/Feature/NotificationPage/Sources/NotificationPageModule/Model/NotificationCellInfo.swift
@@ -6,9 +6,15 @@
 //
 
 import Foundation
-
+import Entity
 
 struct NotificationCellInfo {
     
+    let id: String
+    let isRead: Bool
     
+    let timeText: String
+    let titleText: String
+    let subTitleText: String
+    let imageInfo: ImageDownLoadInfo
 }
--- project/Projects/Presentation/Feature/NotificationPage/Sources/NotificationPageModule/ViewModel/DefualtNotificationCellViewModel.swift
+++ project/Projects/Presentation/Feature/NotificationPage/Sources/NotificationPageModule/ViewModel/DefualtNotificationCellViewModel.swift
@@ -5,10 +5,53 @@
 //  Created by choijunios on 9/28/24.
 //
 
-import Foundation
+import UIKit
 
 
-public class DefualtNotificationCellViewModel {
+import RxSwift
+import RxCocoa
+import PresentationCore
+import ConcreteRepository
+
+class DefualtNotificationCellViewModel: NotificationCellViewModel {
     
+    @Injected var cacheRepository: CacheRepository
     
+    let cellInfo: NotificationCellInfo
+    
+    // Inout
+    var cellClicked: PublishSubject<Void> = .init()
+    
+    // Output
+    var isRead: Driver<Bool>?
+    var profileImage: Driver<UIImage>?
+    
+    let disposeBag: DisposeBag = .init()
+    
+    init(cellInfo: NotificationCellInfo) {
+        self.cellInfo = cellInfo
+        
+        let isReadSubject = BehaviorSubject(value: cellInfo.isRead)
+        
+        // MARK: 읽음 정보
+        isRead = isReadSubject
+            .asDriver(onErrorDriveWith: .never())
+        
+        // MARK: 프로필 이미지
+        profileImage = cacheRepository
+            .getImage(imageInfo: cellInfo.imageInfo)
+            .asDriver(onErrorDriveWith: .never())
+        
+        // MARK: 클릭 이벤트
+        cellClicked
+            .subscribe(onNext: { [isReadSubject] _ in
+                
+                // 읽음 처리
+                isReadSubject.onNext(true)
+                
+                // 알림 디테일로 이동
+                let _ = cellInfo.id
+            })
+            .disposed(by: disposeBag)
+    }
 }
