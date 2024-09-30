743bdef2f104f215ad3aabde99bc405601647957
J0onYEong <cjy2134@naver.com>
2024-09-30 17:16:34 +0900

    [IDLE-000] Router구현체 구현(테스트 미실시)

--- project/Projects/Presentation/Feature/Base/Sources/Router/Router.swift
+++ project/Projects/Presentation/Feature/Base/Sources/Router/Router.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-public protocol Router {
+public protocol RouterProtocol {
     
     typealias Module = UIViewController
     typealias RoutingCompletion = () -> Void
@@ -24,28 +24,156 @@ public protocol Router {
     
     // MARK: push module
     // pop시 호출할 클로저를 여기서 지정(항상 최상단 VC가 팝되지 않음으로)
-    func push(module: Module, transition: UIViewControllerAnimatedTransitioning?, animated: Bool, popCompletion: (() -> Void)?)
+    func push(module: Module, animated: Bool, popCompletion: (() -> Void)?)
     
     
     
     // MARK: pop module
-    func popModule(transition: UIViewControllerAnimatedTransitioning?, animated: Bool)
+    func popModule(animated: Bool)
     
-    /// 전달한 모듈을 해당 스택에서 제거
-    func popModule(module: Module, transition: UIViewControllerAnimatedTransitioning?, animated: Bool)
+    /// 특정 모듈까지 네비게이션 스택을 비움
+    func popTo(module: Module, animated: Bool)
     
     
     
     // MARK: Set root module
     /// 컨트롤러의 루트 VC를 변경
-    func setRootModule(module: Module)
+    func setRootModule(module: Module, animated: Bool)
     
     
     
     // MARK: change window
-    /// 루트 윈도우를 변경, 페인드 인/아웃 애니메이션 적용됨
-    func replaceKeyWindow(module: Module)
+    /// 키 윈도우의 rootController를 변경, 페인드 인/아웃 애니메이션 적용됨
+    func replaceRootModule(module: Module, animated: Bool, completion: RoutingCompletion?)
     
     /// 루트윈도우를 설정
     func setKeyWindow(module: Module)
+}
+
+public final class Router: NSObject, RouterProtocol {
+    
+    weak var rootController: UINavigationController?
+    
+    var completion: [UnsafePointer<Module>: RoutingCompletion] = [:]
+    
+    var transition: UIViewControllerAnimatedTransitioning?
+    
+    /// 네비게이션 최상단ViewController
+    var topViewController: UIViewController? {
+        rootController?.topViewController
+    }
+    
+    public init(rootController: UINavigationController? = nil) {
+        self.rootController = rootController
+    }
+    
+    public func present(_ module: Module, animated: Bool, modalPresentationSytle: UIModalPresentationStyle, completion: RoutingCompletion?) {
+        
+        rootController?.modalPresentationStyle = modalPresentationSytle
+        rootController?.present(
+            module,
+            animated: animated,
+            completion: completion
+        )
+    }
+    
+    public func dismissModule(animated: Bool, completion: (() -> Void)?) {
+        
+        rootController?.dismiss(
+            animated: animated,
+            completion: completion
+        )
+    }
+    
+    public func push(module: Module, animated: Bool, popCompletion: (() -> Void)?) {
+        
+        guard (module is UINavigationController) == false else {
+            // 디버그 모드시 런타임에러발생시킴
+            return assertionFailure("\(#function) 네비게이션 컨트롤러 삽입 불가")
+        }
+        
+        rootController?.pushViewController(
+            module,
+            animated: animated
+        )
+    }
+    
+    public func popModule(animated: Bool) {
+        
+        rootController?
+            .popViewController(animated: animated)
+    }
+    
+    public func popTo(module: Module, animated: Bool) {
+        
+        guard let controllers = rootController?.viewControllers else { return }
+        
+        for controller in rootController!.viewControllers {
+            
+            if controller === module {
+                
+                rootController?.popToViewController(controller, animated: true)
+            }
+        }
+    }
+    
+    public func setRootModule(module: Module, animated: Bool) {
+        
+        rootController?.setViewControllers([module], animated: animated)
+    }
+    
+    public func replaceRootModule(module: Module, animated: Bool, completion: (() -> Void)?) {
+        
+        guard let keyWindow = UIWindow.keyWindow else { return }
+        
+        let navigationController = UINavigationController(rootViewController: module)
+        
+        self.rootController = navigationController
+        
+        if !animated {
+            // 애니메이션이 없는 경우
+            setKeyWindow(module: module)
+            completion?()
+            
+            return
+        }
+        
+        if let snapshot = keyWindow.snapshotView(afterScreenUpdates: true) {
+            
+            module.view.addSubview(snapshot)
+            keyWindow.rootViewController = navigationController
+            
+            completion?()
+            
+            UIView.animate(withDuration: 0.35, animations: {
+                snapshot.layer.opacity = 0
+            }, completion: { _ in
+                snapshot.removeFromSuperview()
+            })
+        }
+    }
+    
+    public func setKeyWindow(module: Module) {
+        guard let keyWindow = UIWindow.keyWindow else { return }
+        let navigationController = UINavigationController(rootViewController: module)
+        
+        keyWindow.rootViewController = navigationController
+        
+        self.rootController = navigationController
+    }
+}
+
+extension UIWindow {
+    
+    static var keyWindow: UIWindow? {
+        
+        if let keyWindow = UIApplication.shared.connectedScenes
+            .compactMap({ $0 as? UIWindowScene })
+            .flatMap({ $0.windows })
+            .first(where: { $0.isKeyWindow }) {
+            
+            return keyWindow
+        }
+        return nil
+    }
 }
