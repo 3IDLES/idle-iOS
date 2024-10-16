52964dbd8091518952e8b8a4da24325cfc68b19f
J0onYEong <cjy2134@naver.com>
2024-10-16 14:38:26 +0900

    [IDLE-000] InputValidationTests위치변경 및 목 객체 주입

--- project/Projects/Testing/Sources/Mock_Data.swift
+++ project/Projects/Testing/Sources/Mock_Data.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Domain
+
+
+import RxSwift
+
+class MockAuthInputValidationRepository: AuthInputValidationRepository {
+    
+    func requestPhoneNumberAuthentication(phoneNumber: String) -> Single<Result<String, DomainError>> {
+        .just(.success(""))
+    }
+    
+    func authenticateAuthNumber(phoneNumber: String, authNumber: String) -> Single<Result<String, DomainError>> {
+        .just(.success(""))
+    }
+    
+    func requestBusinessNumberAuthentication(businessNumber: String) -> Single<Result<BusinessInfoVO, DomainError>> {
+        .just(.success(.mock))
+    }
+    
+    func requestCheckingIdDuplication(id: String) -> Single<Result<Void, DomainError>> {
+        .just(.success(()))
+    }
+}
