cbeae49b491aa41bea5f908ad2b64032719c735b
J0onYEong <cjy2134@naver.com>
2024-10-31 00:13:58 +0900

    [IDLE-000] 오탈자 수정

--- project/Projects/Module/Logger/Sources/MockLogger.swift
+++ project/Projects/Module/Logger/Sources/MockLogger.swift
@@ -12,6 +12,6 @@ public class MockLogger: Logger {
     public init() { }
     
     public func send(_ object: any LoggingObject) {
-        print(object.properties)
+        print("[\(object.eventType)] \(object.properties)")
     }
 }
