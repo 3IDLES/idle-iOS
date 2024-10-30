3a043760ba68d5e8536c767a8149d23a92667efb
J0onYEong <cjy2134@naver.com>
2024-10-30 18:04:13 +0900

    [IDLE-000] 센터구인공고 등록 로깅 리팩토링

--- project/Projects/App/Sources/DIAssembly/LoggerAssembly.swift
+++ project/Projects/App/Sources/DIAssembly/LoggerAssembly.swift
@@ -26,5 +26,6 @@ public struct LoggerAssembly: Assembly {
             
             return AmplitudeLogger()
         }
+        .inObjectScope(.container)
     }
 }
---
+++ project/Projects/Module/Logger/Sources/CreatePost/CreatePostLogBuilder.swift
@@ -0,0 +1,41 @@
+//
+//  CreatePostLogBuilder.swift
+//  Logger
+//
+//  Created by choijunios on 10/30/24.
+//
+
+import Foundation
+
+public struct CreatePostLogObject: LoggingObject {
+    
+    public var properties: [String : Any] {
+        [
+            "step": step,
+            "stepName": stepName
+        ]
+    }
+    
+    let step: Int
+    let stepName: String
+    
+    init(step: Int, stepName: String) {
+        self.step = step
+        self.stepName = stepName
+    }
+}
+
+public class CreatePostLogBuilder {
+    
+    let step: Int
+    let stepName: String
+    
+    public init(step: Int, stepName: String) {
+        self.step = step
+        self.stepName = stepName
+    }
+    
+    public func build() -> AccountRegisterationLogObject {
+        .init(step: step, stepName: stepName)
+    }
+}
