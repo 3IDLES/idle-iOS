651b284c6dd40ef39168351d7a47ab90b69a8f74
J0onYEong <cjy2134@naver.com>
2024-10-31 16:31:24 +0900

    [IDLE-000] 에러 텍스트 일부 업데이트

--- project/Projects/Domain/Sources/Entity/Error/DomainError.swift
+++ project/Projects/Domain/Sources/Entity/Error/DomainError.swift
@@ -129,16 +129,16 @@ public enum DomainError: Error, Equatable {
     public var message: String {
         switch self {
         case .invalidParameter:
-            return "요청하신 API에서 잘못된 파라미터가 입력되었습니다. 입력값을 다시 확인해주세요."
+            return "입력값을 다시 확인해주세요."
             
         case .unAuthorizedRequest:
-            return "접근 권한이 없습니다. 로그인이 필요한 API에 접근하려면 로그인을 먼저 해주세요."
+            return "로그인을 먼저 해주세요."
             
         case .invalidLoginRequest:
-            return "입력하신 ID 또는 비밀번호가 잘못되었습니다. 존재하지 않는 ID로 로그인 시도 시에도 발생할 수 있습니다."
+            return "입력하신 아이디 또는 비밀번호가 잘못되었습니다."
             
         case .invalidPassword:
-            return "비밀번호가 일치하지 않습니다. 정확한 비밀번호를 입력해주세요. (예: 회원 탈퇴 시 비밀번호 입력 단계에서 발생)"
+            return "비밀번호가 일치하지 않습니다. 정확한 비밀번호를 입력해주세요."
             
         case .unregisteredUser:
             return "등록되지 않은 사용자입니다. 회원가입이 필요합니다."
@@ -153,7 +153,7 @@ public enum DomainError: Error, Equatable {
             return "유효하지 않은 토큰입니다. 토큰의 값이 올바른지 다시 확인해주세요."
             
         case .tokenExpiredException:
-            return "토큰이 만료되었습니다. 다시 로그인해주세요."
+            return "로그인 지속시간을 초과했습니다. 다시 로그인해주세요."
             
         case .tokenNotFound:
             return "토큰을 찾을 수 없습니다. 요청을 다시 확인해주세요."
@@ -189,10 +189,10 @@ public enum DomainError: Error, Equatable {
             return "요청하신 리소스를 찾을 수 없습니다. 요청이 올바른지 다시 확인해주세요."
             
         case .clientException:
-            return "SMS 발송에 실패했습니다. 입력된 정보를 다시 확인하거나 나중에 다시 시도해주세요."
+            return "인증번호 메세지 발송에 실패했습니다."
             
         case .businessCodeNotFound:
-            return "사업자 등록번호를 찾을 수 없습니다. 정확한 정보를 입력했는지 확인해주세요."
+            return "사업자 등록번호를 찾을 수 없습니다."
             
         case .geoCodingFailure:
             return "입력된 주소로 지리 정보를 찾을 수 없습니다. 주소를 다시 확인해주세요."
