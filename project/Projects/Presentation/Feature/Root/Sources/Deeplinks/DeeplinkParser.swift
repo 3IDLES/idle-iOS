8d94cda2676a91b3f4b4f2c3770e141d005f6a89
J0onYEong <cjy2134@naver.com>
2024-11-13 16:31:33 +0900

    [IDLE-515] 요양보호사가 알림을 통해 공고로 바로이동하는 기능 구현

--- project/Projects/Presentation/Feature/Root/Sources/Deeplinks/DeeplinkParser.swift
+++ project/Projects/Presentation/Feature/Root/Sources/Deeplinks/DeeplinkParser.swift
@@ -27,6 +27,9 @@ enum DeeplinkParserError: LocalizedError {
 
 class DeeplinkParser {
     
+    /// [딥링크 동작]
+    /// 먼저 딥링크를 처리할 수 있는 루트와 스타팅 포인트를 탐색합니다. 스타팅포인트는 옵셔널 입니다.
+    /// 스타팅 포인트를 따로둔 이유는 알림 인앱처리시 루트가 코디네이터에 대한 처리가 필요없기 때문입니다.
     func makeDeeplinkList(components: [DeepLinkPathComponent], startFromRoot: Bool = true) throws -> [DeeplinkExecutable] {
         
         var deeplinks: [DeeplinkExecutable] = []
@@ -35,12 +38,14 @@ class DeeplinkParser {
             
             if deeplinks.isEmpty {
                 
+                // 딥링크의 경로의 첫번째 시작지점을 선정합니다.
+                
                 var start: DeeplinkExecutable!
                 
                 if startFromRoot {
                     start = try findRoot(component: component)
                 } else {
-                    start = try findBranchPoint(component: component)
+                    start = try findFirstStartPointAboveApp(component: component)
                 }
                 
                 deeplinks.append(start)
@@ -57,6 +62,7 @@ class DeeplinkParser {
         return deeplinks
     }
     
+    /// 딥링크의 루트를 의미합니다.
     private func findRoot(component: DeepLinkPathComponent) throws -> DeeplinkExecutable {
         switch component {
         case .centerMainPage:
@@ -68,14 +74,17 @@ class DeeplinkParser {
         }
     }
     
-    private func findBranchPoint(component: DeepLinkPathComponent) throws -> DeeplinkExecutable {
+    /// 루트는 아니지만 스타팅 포인트가 될 수 있는 지점을 의미합니다.
+    private func findFirstStartPointAboveApp(component: DeepLinkPathComponent) throws -> DeeplinkExecutable {
         switch component {
-        case .centerMainPage:
-            return CenterMainPageDeeplink()
         case .postApplicantPage:
+            
             return PostApplicantDeeplink()
-        case .workerMainPage:
-            return CenterMainPageDeeplink()
+            
+        case .postDetailForWorkerPage:
+            
+            return PostDetailForWorkerDeepLink()
+            
         default:
             throw DeeplinkParserError.startPointNotFound
         }
